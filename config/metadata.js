e295b0a667b4d612971ee8572df5305a4a7bab19
RgMellon <renan_gian@hotmail.com>
2020-03-28 09:45:55 -0300

    first commit

---
+++ config/metadata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: `coffy-code`,
+  siteUrl: `https://www.coffy-code.com`,
+  description: `Construa seu aplicativo conosco :)`,
+}
---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+const siteMetadata = require("./config/metadata")
+
+module.exports = {
+  siteMetadata,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#DDC79E`,
+        showSpinner: false,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`300`, `500`, `700`],
+          },
+        ],
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `CofyCode`,
+        short_name: `Cofy`,
+        start_url: `/`,
+        background_color: `#DDC79E`,
+        theme_color: `#40433D`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-react-helmet`,
+
+    `gatsby-plugin-offline`,
+  ],
+}
